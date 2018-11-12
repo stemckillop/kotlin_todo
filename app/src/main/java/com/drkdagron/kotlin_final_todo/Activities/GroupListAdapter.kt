67e3c17b939eda6df8343fc2f2949d5aa50df2d7
Stephen McKillop <smckillop@innovada.ca>
2018-11-12 16:00:25 -0500

    added menu to groups

--- app/src/main/java/com/drkdagron/kotlin_final_todo/Activities/GroupListAdapter.kt
+++ app/src/main/java/com/drkdagron/kotlin_final_todo/Activities/GroupListAdapter.kt
@@ -1,10 +1,13 @@
 package com.drkdagron.kotlin_final_todo.Activities
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.RelativeLayout
 import android.widget.TextView
+import android.widget.Toast
 import com.drkdagron.kotlin_final_todo.R
 import com.drkdagron.kotlin_final_todo.db.Group
 import kotlinx.android.synthetic.main.list_group.view.*
@@ -17,6 +20,10 @@ class GroupListAdapter(var data: MutableList<Group>) :
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder{
         val view = LayoutInflater.from(p0.context)
                 .inflate(R.layout.list_group, p0, false) as RelativeLayout
+
+        view.findViewById<ImageButton>(R.id.list_group_extra).setOnClickListener {
+            Toast.makeText(p0.context, "Menu button clicked", Toast.LENGTH_SHORT).show()
+        }
 
         return ViewHolder(view)
     }
